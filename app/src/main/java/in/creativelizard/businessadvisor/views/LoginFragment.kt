9d631194d6b9aa754d42af0512b243c1ec6663c6
siddharthamaji <siddhartha.maji@switch2web.com>
2019-09-06 16:15:49 +0530

    new Page

--- app/src/main/java/in/creativelizard/businessadvisor/views/LoginFragment.kt
+++ app/src/main/java/in/creativelizard/businessadvisor/views/LoginFragment.kt
@@ -3,10 +3,12 @@ package `in`.creativelizard.businessadvisor.views
 
 import `in`.creativelizard.businessadvisor.R
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_login.view.*
 
@@ -26,6 +28,10 @@ class LoginFragment : Fragment() {
     private fun onActionPerform() {
         rootView.fabNext.setOnClickListener {
             (it as FloatingActionButton).hide()
+
+            Handler().postDelayed({
+                Navigation.findNavController(rootView).navigate(R.id.action_loginFragment_to_formFragment)
+            },1000)
 
         }
     }
