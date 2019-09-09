0a2e49a759e4ace968533b0c23d82445b64471c3
siddharthamaji <siddhartha.maji@switch2web.com>
2019-09-09 19:35:28 +0530

    new Page

--- app/src/main/java/in/creativelizard/businessadvisor/views/utils/AppUtils.kt
+++ app/src/main/java/in/creativelizard/businessadvisor/views/utils/AppUtils.kt
@@ -1,7 +1,21 @@
 package `in`.creativelizard.businessadvisor.views.utils
 
+import android.app.TimePickerDialog
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import java.util.*
 
 class AppUtils {
+    companion object{
+        fun getTime(it: View?) {
+            val timeDialog = TimePickerDialog(it?.context,
+                TimePickerDialog.OnTimeSetListener { v, hourOfDay, minute ->
+                    (it as TextView).text = "$hourOfDay : $minute"
+                }, Calendar.HOUR_OF_DAY, Calendar.MINUTE, false
+            )
+            timeDialog.show()
+        }
+    }
 
 }
