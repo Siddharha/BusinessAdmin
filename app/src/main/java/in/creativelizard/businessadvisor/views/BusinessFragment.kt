9be1777ae1ce842a2d7502fc78b0b04d66e89c95
siddharthamaji <siddhartha.maji@switch2web.com>
2019-09-11 12:27:51 +0530

    new Page

--- app/src/main/java/in/creativelizard/businessadvisor/views/BusinessFragment.kt
+++ app/src/main/java/in/creativelizard/businessadvisor/views/BusinessFragment.kt
@@ -5,6 +5,7 @@ import `in`.creativelizard.businessadvisor.R
 import `in`.creativelizard.businessadvisor.views.utils.AppUtils
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -21,8 +22,18 @@ class BusinessFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_business, container, false)
+        initialize()
         onActionPerform()
         return rootView
+    }
+
+    private fun initialize() {
+        rootView.clBusiness.translationX =  resources.displayMetrics.widthPixels.toFloat()
+
+        Handler().postDelayed({
+            rootView.clBusiness.animate().translationX(1f).duration = 500
+        },100)
+
     }
 
     @SuppressLint("SetTextI18n")
