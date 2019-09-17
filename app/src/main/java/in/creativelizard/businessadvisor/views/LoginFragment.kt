1997d697cb278902dd9f05b99a9f8934f7632c8b
siddharthamaji <siddhartha.maji@switch2web.com>
2019-09-17 17:31:38 +0530

    new Page

--- app/src/main/java/in/creativelizard/businessadvisor/views/LoginFragment.kt
+++ app/src/main/java/in/creativelizard/businessadvisor/views/LoginFragment.kt
@@ -6,6 +6,8 @@ import `in`.creativelizard.businessadvisor.models.networkModels.LoginOutput
 import `in`.creativelizard.businessadvisor.viewModels.LoginViewModel
 import android.os.Bundle
 import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +19,7 @@ import androidx.navigation.Navigation
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_login.view.*
+import java.nio.file.WatchEvent
 
 
 class LoginFragment : Fragment() {
@@ -35,9 +38,26 @@ class LoginFragment : Fragment() {
 
     private fun initialize() {
         loginViewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+
     }
 
     private fun onActionPerform() {
+
+        rootView.etTokenId.addTextChangedListener(object :TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                rootView.tvLength.text = "${s?.length}/30"
+            }
+
+        })
+
         rootView.fabNext.setOnClickListener {
             (it as FloatingActionButton).hide()
 
@@ -50,6 +70,7 @@ class LoginFragment : Fragment() {
                             Navigation.findNavController(rootView).navigate(R.id.action_loginFragment_to_formFragment)
                         },1000)
                     }else{
+                        it.show()
                         Toast.makeText(activity!!,loginData.message,Toast.LENGTH_SHORT).show()
                     }
                 })
